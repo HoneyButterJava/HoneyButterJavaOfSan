e1018a8c6f83ce883839587e7140c9d672c3d6c0
anso33 <hyeon131812@gmail.com>
2023-03-14 02:05:14 +0900

    COMMENT: add comment

--- src/greedy/pg_42862.java
+++ src/greedy/pg_42862.java
@@ -13,8 +13,12 @@ public class pg_42862 {
 		int answer = 0;
 		int lostIndex = 0;
 		int reserveIndex = 0;
+//		여러 배열을 함께 봐야하기때문에 인덱스를 따로 둠.
 		for (int i = 0; i < n; i++) {
 			student[i] = 1;
+
+//			reserve였어도 lost면 개수를 빼야하기 때문에 reserve에 대한 값을 먼저 적용하고 lost를 적용했습니다.
+//			둘다 확인해야 하기 때문에 그냥 조건에만 맞으면 확인하도록 if로 나눴습니다.
 			if (reserveIndex < reserve.length && reserve[reserveIndex] - 1 == i) {
 				student[i] = 2;
 				reserveIndex++;
@@ -23,13 +27,14 @@ public class pg_42862 {
 				student[i]--;
 				lostIndex++;
 			}
+//			기본적으로 지금 체육복을 1개 이상 가지고 있으면 체육복을 입을 수 있기 떄문에 answer up
 			if (student[i] > 0)
 				answer++;
 		}
-		for (int i = 0; i < n ; i++)
-			System.out.print(student[i] +" ");
 
+//		이제 체육복을 안가져온 불량 학생들에게 빌려줍니다. 빌린 학생들은 answer up 할겁니다.
 		for (int i = 0; i < n; i++) {
+//		지금 체육복이 2개다? 그럼 빌려줄 애가 있는지 앞에부터 확인해봅니다.
 			if (student[i] == 2) {
 				if (i > 0 && student[i - 1] == 0) {
 					student[i - 1]++;
@@ -42,7 +47,6 @@ public class pg_42862 {
 				answer++;
 			}
 		}
-
 		return answer;
 	}
 
